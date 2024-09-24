16599ac5d29bd050aa7e3047bfede6ef01e643c6
Dylan Chan <chan0566@algonquinlive.com>
2024-09-24 17:28:57 -0400

    My first commit

---
+++ bin/hybrid_ex4.dart
@@ -0,0 +1,5 @@
+import 'package:hybrid_ex4/hybrid_ex4.dart' as hybrid_ex4;
+
+void main(List<String> arguments) {
+  print('Hello world: ${hybrid_ex4.calculate()}!');
+}
---
+++ test/hybrid_ex4_test.dart
@@ -0,0 +1,8 @@
+import 'package:hybrid_ex4/hybrid_ex4.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
